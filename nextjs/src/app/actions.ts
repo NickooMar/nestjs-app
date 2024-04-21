a05c28f7d95c2c7bee153cbab8e9d264d7414244
NickooMar <nicoo.marsili@gmail.com>
2024-04-20 21:51:02 -0300

    fix: nextjs auth

--- nextjs/src/app/actions.ts
+++ nextjs/src/app/actions.ts
@@ -3,5 +3,7 @@
 import { signIn } from "@/auth"
 
 export async function signinProvidersAction(provider: string) {
+  if (!provider || provider !== "Google") throw new Error("Invalid provider")
+
   await signIn(provider)
 }
